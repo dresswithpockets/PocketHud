b7fee0ef17263f1fb7d0c19289ea4f2cd9376dbf
Ashley <dresswithpockets@pm.me>
2021-07-13 21:37:25 -0700

    add structs and base functionality for menu groups and elements

--- mainmenu.go
+++ mainmenu.go
@@ -1,1 +1,118 @@
 package main
+
+// MainMenuOverride defined the area of the screen that the main menu occupies
+type MainMenuOverride struct {
+    BaseControl
+    updateUrl     string `vgui:"update_url"`
+    blogUrl       string `vgui:"blog_url"`
+    buttonXOffset int    `vgui:"button_x_offset"`
+    buttonY       int    `vgui:"button_y"`
+    buttonYDelta  int    `vgui:"button_y_delta"`
+}
+
+// KvButton child control for MainMenuOverride
+// defines the style and layout for the buttons in gamemenu.res that arent explicitly defined
+type KvButton struct {
+    BaseControl
+}
+
+type SaxxySettings struct {
+    BaseControl
+    autoResize              bool
+    pinCorner               bool
+    flashBounds             Bounds
+    flashStartSizeMin       int
+    flashStartSizeMax       int
+    flashMaxScale           int
+    flashLifeLengthMin      float32
+    flashLifeLengthMax      float32
+    curtainAnimDuration     float32
+    curtainOpenTime         float32
+    flashStartTime          float32
+    initialFreakoutDuration float32
+    clapSoundDuration       float32
+    cameraFlashSettings     CameraFlashSettings
+}
+
+type CameraFlashSettings struct {
+    BaseControl
+    tileImage  bool
+    scaleImage bool
+}
+
+type CPvPRankPanel struct {
+    EditablePanel
+    // TODO: functionality
+}
+
+type CSteamFriendsListPanel struct {
+    BaseControl
+    // TODO: what base type?
+}
+
+type ScrollBar struct {
+    BaseControl
+    // TODO: what base type?
+}
+
+type ScrollableEditablePanel struct {
+    EditablePanel
+    // TODO: functionality
+}
+
+type CMainMenuNotificationsControl struct {
+    BaseControl
+    // TODO: what base type?
+}
+
+type CTFStreamListPanel struct {
+    BaseControl
+    // TODO: what base type?
+}
+
+type CItemModelPanel struct {
+    BaseControl
+    // TODO: what base type?
+}
+
+// TODO: from vgui.Value
+
+func (m *MainMenuOverride) draw() {
+    panic("Draw is not implemented for MainMenuOverride yet.")
+}
+
+func (k *KvButton) draw() {
+    panic("Draw is not implemented for KvButton yet.")
+}
+
+func (s *SaxxySettings) draw() {
+    panic("Draw is not implemented for SaxxySettings yet.")
+}
+
+func (c *CPvPRankPanel) draw() {
+    panic("Draw is not implemented for CPvPRankPanel yet.")
+}
+
+func (c *CSteamFriendsListPanel) draw() {
+    panic("Draw is not implemented for CStreamFriendsListPanel yet.")
+}
+
+func (s *ScrollBar) draw() {
+    panic("Draw is not implemented for ScrollBar yet.")
+}
+
+func (s *ScrollableEditablePanel) draw() {
+    panic("Draw is not implemented for ScrollableEditablePanel yet.")
+}
+
+func (c *CMainMenuNotificationsControl) draw() {
+    panic("Draw is not implemented for CMainMenuNotificationsControl yet.")
+}
+
+func (c *CTFStreamListPanel) draw() {
+    panic("Draw is not implemented for CTFStreamListPanel yet.")
+}
+
+func (c *CItemModelPanel) draw() {
+    panic("Draw is not implemented for CItemModelPanel yet.")
+}
--- menu.go
+++ menu.go
@@ -1,15 +1,38 @@
-package menus
+package main
 
-import ".."
+import "github.com/dresswithpockets/go-vgui"
 
-type Menu interface {
-    drawMenu(app *main.App)
+type Element struct {
+    name     string
+    controls map[string]Control
 }
 
-type MainMenu struct {
-
+type Menu struct {
+    file     string
+    elements map[string]*Element
 }
 
-func (m *MainMenu) drawMenu(app *main.App) {
+func (c *ControlProvider) newElementFromObject(object *vgui.Object) *Element {
+    controls := map[string]Control{}
+    for k, v := range object.Properties {
+        control, err := c.resolveNewControlFromObject(v)
+        if err != nil {
+            // TODO: handle erroneous control from object
+        }
+        controls[k] = control
+    }
+    return &Element{object.Name, controls}
+}
 
+func (c *ControlProvider) newMenuFromObject(object *vgui.Object) *Menu {
+    elements := map[string]*Element{}
+    for k, v := range object.Properties {
+        elements[k] = c.newElementFromObject(v)
+    }
+    // TODO: get file from object
+    return &Menu{"", elements}
+}
+
+func (b *Menu) drawMenu(app *App) {
+    panic("Draw not implemented on Menu.")
 }
