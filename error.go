2a654735bacd0e0b7b21488b714072bda29bdbaa
Ashley <dresswithpockets@pm.me>
2021-07-13 21:35:50 -0700

    base error type for failed control resolution

--- error.go
+++ error.go
@@ -1,1 +1,11 @@
 package main
+
+import "fmt"
+
+type ErrUnknownControlName struct {
+    controlName string
+}
+
+func (e *ErrUnknownControlName) Error() string {
+    return fmt.Sprintf("There is no builder associated with the control name/type of '%v'", e.controlName)
+}
